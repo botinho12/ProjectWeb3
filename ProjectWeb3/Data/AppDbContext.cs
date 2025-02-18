65579912d19967b7b682f25c8966572ba6fcc88a
botinho12 <Botivinicios77@gmail.com>
2025-02-17 22:28:35 -0300

    Criando as migrations

--- ProjectWeb3/Data/AppDbContext.cs
+++ ProjectWeb3/Data/AppDbContext.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProjectWeb3.Models;
@@ -18,5 +15,82 @@ namespace ProjectWeb3.Data
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<ProdutoFoto> ProdutoFoto { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            #region Definição dos nomes do Entity  
+            builder.Entity<Usuario>().ToTable("usuario");
+            builder.Entity<IdentityRole>().ToTable("perfil");
+            builder.Entity<IdentityUserRole<string>>().ToTable("usuario_perfil");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("usuario_regra");
+            builder.Entity<IdentityUserToken<string>>().ToTable("usuario_token");        
+            builder.Entity<IdentityUserLogin<string>>().ToTable("usuario_login");        
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("perfil_regra");       
+            #endregion
+
+            #region Categorias
+                
+                List<Categoria> categorias = new(){
+                    new(){
+                        Id = 1,
+                        Name = "Eletrônicos", 
+                    },
+                    new(){
+                        Id = 2,
+                        Name = "Celulares"
+                    }
+                };
+                builder.Entity<Categoria>().HasData(categorias);
+            #endregion
+            #region Popular Usuario
+
+            Usuario usuario = new(){
+                Id = Guid.NewGuid().ToString(),
+                UserName = "BotinhoLindo",
+                NormalizedUserName = "BOTINHOLINDO",
+                Email = "BotiLoko12@gmail.com",
+                NormalizedEmail = "BOTILOKO12@GMAIL.COM",
+                EmailConfirmed = true,
+                Name = "Vinicius augusto boti pereira",
+                DataNascimento = DateTime.Parse("06/09/2002"),
+                LockoutEnabled = true                
+            };
+            PasswordHasher<Usuario> password = new();
+            password.HashPassword(usuario,"12345");
+            builder.Entity<Usuario>().HasData(usuario);
+            #endregion
+           
+            #region Popular Perfil
+            List<IdentityRole> perfis = new(){
+                new(){
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Administrador",
+                    NormalizedName = "ADMINISTRADOR"
+                },
+                new(){
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "funcionario",
+                    NormalizedName = "FUNCIONARIO"
+                },
+                new(){
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "cliente",
+                    NormalizedName = "CLIENTE"
+                }        
+            };
+            builder.Entity<IdentityRole>().HasData(perfis);               
+            #endregion
+
+            #region Popular Usuário-Perfil
+            List<IdentityUserRole<string>> userRoles = new(){
+                new(){
+                    UserId = usuario.Id,
+                    RoleId = perfis[0].Id
+                }
+            };
+            builder.Entity<IdentityUserRole<string>>().HasData(userRoles);               
+            #endregion
+        }    
     }
 }
--- ProjectWeb3/Program.cs
+++ ProjectWeb3/Program.cs
@@ -1,9 +1,33 @@
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ProjectWeb3.Data;
+using ProjectWeb3.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+string conexao = builder.Configuration.GetConnectionString("ProjectWeb");
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(conexao)
+);
+
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    options => options.SignIn.RequireConfirmedEmail = false
+).AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+//Garantir que o banco exista ao executar o projeto
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+} 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
