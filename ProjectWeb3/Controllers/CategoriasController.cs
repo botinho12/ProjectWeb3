8265dff616271086f5ad30f5a37687494cd5920a
Botinho12 <botivinicios77@gmail.com>
2025-03-21 22:36:31 -0300

    Alterando a CategoriaController para ulitlizar tempData e arrumando suas views

--- ProjectWeb3/Controllers/CategoriasController.cs
+++ ProjectWeb3/Controllers/CategoriasController.cs
@@ -65,17 +65,17 @@ namespace ProjectWeb3.Controllers
 
                 if (Arquivo != null)
                 {
-                    string nomeArquivo = categoria.Id + Path.GetExtension(Arquivo.FileName);
+                    string filename = categoria.Id + Path.GetExtension(Arquivo.FileName);
                     string caminho = Path.Combine(_host.WebRootPath, "img\\categorias");
-                    string novoArquivo = Path.Combine(caminho, nomeArquivo);
-                    using (FileStream stream = new FileStream(novoArquivo, FileMode.Create))
+                    string novoArquivo = Path.Combine(caminho, filename);
+                    using (var stream = new FileStream(novoArquivo, FileMode.Create))
                     {
                         Arquivo.CopyTo(stream);
                     }
-                    categoria.Foto = "\\img\\categorias\\" + nomeArquivo;
+                    categoria.Foto = "\\img\\categorias\\" + filename;
                     await _context.SaveChangesAsync();
                 }
-
+                TempData["Success"] = "Categoria Cadastrada com Sucesso!";
                 return RedirectToAction(nameof(Index));
             }
             return View(categoria);
@@ -102,7 +102,7 @@ namespace ProjectWeb3.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Foto")] Categoria categoria)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Foto")] Categoria categoria, IFormFile Arquivo)
         {
             if (id != categoria.Id)
             {
@@ -113,6 +113,17 @@ namespace ProjectWeb3.Controllers
             {
                 try
                 {
+                    if (Arquivo != null)
+                    {
+                        string filename = categoria.Id + Path.GetExtension(Arquivo.FileName);
+                        string caminho = Path.Combine(_host.WebRootPath, "img\\categorias");
+                        string novoArquivo = Path.Combine(caminho, filename);
+                        using (var stream = new FileStream(novoArquivo, FileMode.Create))
+                        {
+                            Arquivo.CopyTo(stream);
+                        }
+                        categoria.Foto = "\\img\\categorias\\" + filename;
+                    }
                     _context.Update(categoria);
                     await _context.SaveChangesAsync();
                 }
@@ -127,6 +138,7 @@ namespace ProjectWeb3.Controllers
                         throw;
                     }
                 }
+                TempData["Success"] = "Categoria Alterada com Sucesso!";
                 return RedirectToAction(nameof(Index));
             }
             return View(categoria);
@@ -162,6 +174,7 @@ namespace ProjectWeb3.Controllers
             }
 
             await _context.SaveChangesAsync();
+            TempData["Success"] = "Categoria Excluida com Sucesso!";
             return RedirectToAction(nameof(Index));
         }
 
